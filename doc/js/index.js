43144ddcdecb2f62b671a57db17a3d193bef3e05
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2024-08-04 20:43:15 +0200

    doc: improve documentation UX

--- doc/js/index.js
+++ doc/js/index.js
@@ -1,10 +1,23 @@
 import { rx } from '@toonvanvr/rx-ify'
 import { interval, map, tap } from 'rxjs'
 
+// Make the counter thumnail interactive
 const prevCounterValue = Number(localStorage.getItem('counter') || 0)
-
 const div = document.getElementById('counter')
 rx(div).innerHTML = interval(500).pipe(
   map((i) => prevCounterValue + i),
   tap((i) => localStorage.setItem('counter', i))
 )
+
+// Remember closing an alert
+document.querySelectorAll('sl-alert[data-local-storage-key]').forEach(async (alert) => {
+  if (!(alert instanceof HTMLElement)) return
+
+  if (localStorage.getItem(alert.dataset.localStorageKey) !== 'closed') {
+    alert.style.display = 'block'
+  }
+
+  alert.addEventListener('sl-hide', () => {
+    localStorage.setItem(alert.dataset.localStorageKey, 'closed')
+  })
+})
