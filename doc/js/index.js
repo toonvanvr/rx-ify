65de9005ccb2170eaf8fb2d1877155a120d555d6
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2024-08-04 22:08:49 +0200

    doc: no glitch when starting to count in the cards view
    not going to fix the minimal code example to keep it as minimal as possible

--- doc/js/index.js
+++ doc/js/index.js
@@ -4,8 +4,9 @@ import { interval, map, tap } from 'rxjs'
 // Make the counter thumnail interactive
 const prevCounterValue = Number(localStorage.getItem('counter') || 0)
 const div = document.getElementById('counter')
+div.innerText = prevCounterValue
 rx(div).innerHTML = interval(500).pipe(
-  map((i) => prevCounterValue + i),
+  map((i) => prevCounterValue + i + 1),
   tap((i) => localStorage.setItem('counter', i))
 )
 
