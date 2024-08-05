ba822d52a5c238529a34adfd164a397b1a85c331
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2024-08-05 20:03:32 +0200

    doc: fix demo import

--- doc/js/demo/minimal.js
+++ doc/js/demo/minimal.js
@@ -1,11 +1,11 @@
-import { rx } from '@toonvanvr/rx-ify'
+import { $$ } from '@toonvanvr/rx-ify'
 import { interval, map, tap } from 'rxjs'
 
 // Make the counter thumnail interactive
 const prevCounterValue = Number(localStorage.getItem('counter') || 0)
 const div = document.getElementById('counter')
 div.innerText = prevCounterValue
-rx(div).innerHTML = interval(500).pipe(
+$$(div).innerHTML = interval(500).pipe(
   map((i) => prevCounterValue + i + 1),
   tap((i) => localStorage.setItem('counter', i))
 )
